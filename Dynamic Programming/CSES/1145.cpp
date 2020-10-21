4cb44a0293f8a14504397c528ba2e92046307271
Abhishek Tiwari <39875465+becomeahacker@users.noreply.github.com>
2020-10-22 04:17:12 +0530

    Add solutions to all CSES DP problems

---
+++ Dynamic Programming/CSES/1145.cpp
@@ -0,0 +1,190 @@
+
+// Problem : Increasing Subsequence
+// Contest : CSES - CSES Problem Set
+// URL : https://cses.fi/problemset/task/1145
+// Memory Limit : 512 MB
+// Time Limit : 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+// Code by : Abhishek Tiwari @devil_within
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define all(v) (v).begin(), (v).end()
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define vt vector<int>
+#define pii pair<int, int>
+#define ar array
+using namespace std;
+
+using namespace __gnu_pbds;
+template <class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define int int64_t
+using ll = long long int;
+using ld = long double;
+
+/*******************************************************************************************/
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v);
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v);
+template <typename T> inline istream &operator>>(istream &in, vector<T> &a);
+/*******************************************************************************************/
+// N dimensional Vector, Vec<n , int>
+template <int D, typename T> struct Vec : public vector<Vec<D - 1, T>>
+{
+    static_assert(D >= 1, "Vector dimension must be greater than zero!");
+    template <typename... Args> Vec(int n = 0, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...))
+    {
+    }
+};
+template <typename T> struct Vec<1, T> : public vector<T>
+{
+    Vec(int n = 0, const T &val = T()) : vector<T>(n, val)
+    {
+    }
+};
+/*******************************************************************************************/
+ll fpow(ll x, ll y, ll p = LONG_LONG_MAX)
+{
+    ll res = 1;
+    x = x % p;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
+ll randint(ll a, ll b)
+{
+    return uniform_int_distribution<ll>(a, b)(mt_rng);
+}
+
+const int R4[4] = {-1, 0, 1, 0}, C4[4] = {0, 1, 0, -1};
+const int R8[8] = {-1, -1, 0, 1, 1, 1, 0, -1}, C8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+const ll INF = LONG_LONG_MAX;
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector< int > a(n);
+    cin >> a;
+    
+    // Current LIS length
+    int len = 1;
+    
+    // active[i] = end element of LIS of length i
+    // It will be in ascending order due to our algo
+    vector< int > active(n , 0);
+    active[0] = a[0];
+    // cout << a;
+    
+	for(int i = 1; i < a.size(); ++i) {
+		if( a[i] <= active[0] ) 
+			active[0] = a[i];
+		else if( a[i] > active[ len - 1 ] ) 
+			active[ len++ ] = a[i];
+		else {
+			//Value is in between binary search and find 
+			// the rightmost list with last element < a[i] , extend that list 
+			// so that you get a new list with lower end element than what 
+			// was prev there
+			int lo = 0 , hi = len - 1;
+			int idx = 0;
+			while (lo <= hi) {
+				int mid = (lo + hi)/2;
+				if(active[mid] < a[i] ) {
+					idx = mid;
+					lo = mid + 1;
+				} else {
+					hi = mid - 1;
+				}
+			}
+			// Replace the next list
+			active[idx + 1] = a[i];
+		}
+		// cout << active ;
+	}
+	cout << len << endl;
+
+}
+
+signed main()
+{
+    // freopen("input.txt" , "r" , stdin);freopen("output.txt" , "w" ,stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // cout << fixed << setprecision(12);
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; ++tc)
+    {
+        // cout << "Case #"<<tc<<": \n";
+        solve();
+    }
+}
+
+// Functions
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (int i = 0; i < (int)v.size(); ++i)
+    {
+        if (i)
+            os << ", ";
+        os << v[i];
+    }
+    os << "]\n";
+    return os;
+}
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v)
+{
+    os << "{";
+    for (auto it : v)
+    {
+        os << it;
+        if (it != *v.rbegin())
+            os << ", ";
+    }
+    os << "}\n";
+    return os;
+}
+template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v)
+{
+    os << "{";
+    for (auto it : v)
+    {
+        os << "(" << it.first << " : " << it.second << ")";
+        if (it != *v.rbegin())
+            os << ", ";
+    }
+    os << "}\n";
+    return os;
+}
+template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v)
+{
+    os << "(";
+    os << v.first << ", " << v.second << ")";
+    return os;
+}
+template <typename T> inline istream &operator>>(istream &in, vector<T> &a)
+{
+    for (auto &x : a)
+        in >> x;
+    return in;
+}
---
+++ Dynamic Programming/CSES/1746.cpp
@@ -0,0 +1,184 @@
+
+// Problem : Array Description
+// Contest : CSES - CSES Problem Set
+// URL : https://cses.fi/problemset/task/1746
+// Memory Limit : 512 MB
+// Time Limit : 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+// Code by : Abhishek Tiwari @devil_within
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define all(v) (v).begin(), (v).end()
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define vt vector<int>
+#define pii pair<int, int>
+#define ar array
+using namespace std;
+
+using namespace __gnu_pbds;
+template <class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// #define int int64_t
+using ll = long long int;
+using ld = long double;
+
+/*******************************************************************************************/
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v);
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v);
+template <typename T> inline istream &operator>>(istream &in, vector<T> &a);
+/*******************************************************************************************/
+// N dimensional Vector, Vec<n , int>
+template <int D, typename T> struct Vec : public vector<Vec<D - 1, T>>
+{
+    static_assert(D >= 1, "Vector dimension must be greater than zero!");
+    template <typename... Args> Vec(int n = 0, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...))
+    {
+    }
+};
+template <typename T> struct Vec<1, T> : public vector<T>
+{
+    Vec(int n = 0, const T &val = T()) : vector<T>(n, val)
+    {
+    }
+};
+/*******************************************************************************************/
+ll fpow(ll x, ll y, ll p = LONG_LONG_MAX)
+{
+    ll res = 1;
+    x = x % p;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
+ll randint(ll a, ll b)
+{
+    return uniform_int_distribution<ll>(a, b)(mt_rng);
+}
+
+const int R4[4] = {-1, 0, 1, 0}, C4[4] = {0, 1, 0, -1};
+const int R8[8] = {-1, -1, 0, 1, 1, 1, 0, -1}, C8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+const ll INF = LONG_LONG_MAX;
+const int MOD = 1e9 + 7;
+
+const int MXN = 1e5 + 1;
+const int MXM = 1e2 + 5;
+
+ll dp[MXN][MXM];
+
+void solve()
+{
+    int n , m;
+    cin >>n >> m;
+    vector< int > a(n);
+    cin >> a;
+    
+    if(a[0] == 0) 
+    	for(int i = 1; i <=m; ++i) 
+    		dp[1][i] = 1;
+    else 
+    	dp[1][a[0]] = 1;
+    	
+    for(int i = 2; i <= n; ++i ) {
+    	if(a[i-1] != 0 ) {
+    		dp[i][a[i-1]] = dp[i-1][a[i-1]] + dp[i-1][a[i-1] - 1] + dp[i-1][a[i-1] + 1];
+    		dp[i][a[i-1]] %= MOD;	
+    		continue;
+    	} 
+    	
+    	for(int j = 1; j <= m; ++j ) {
+    		dp[i][j] = dp[i-1][j] + dp[i-1][j-1] + dp[i-1][j+1];
+    		dp[i][j] %= MOD;
+    	}
+    }
+    
+    ll ans = 0;
+    for(int i = 1; i <= m; ++i) 	
+    	ans = (ans + (ll)dp[n][i])%MOD;
+    cout << ans << endl;
+    
+    
+    
+}
+
+signed main()
+{
+    // freopen("input.txt" , "r" , stdin);freopen("output.txt" , "w" ,stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // cout << fixed << setprecision(12);
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; ++tc)
+    {
+        // cout << "Case #"<<tc<<": \n";
+        solve();
+    }
+}
+
+// Functions
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (int i = 0; i < (int)v.size(); ++i)
+    {
+        if (i)
+            os << ", ";
+        os << v[i];
+    }
+    os << "]\n";
+    return os;
+}
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v)
+{
+    os << "{";
+    for (auto it : v)
+    {
+        os << it;
+        if (it != *v.rbegin())
+            os << ", ";
+    }
+    os << "}\n";
+    return os;
+}
+template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v)
+{
+    os << "{";
+    for (auto it : v)
+    {
+        os << "(" << it.first << " : " << it.second << ")";
+        if (it != *v.rbegin())
+            os << ", ";
+    }
+    os << "}\n";
+    return os;
+}
+template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v)
+{
+    os << "(";
+    os << v.first << ", " << v.second << ")";
+    return os;
+}
+template <typename T> inline istream &operator>>(istream &in, vector<T> &a)
+{
+    for (auto &x : a)
+        in >> x;
+    return in;
+}
