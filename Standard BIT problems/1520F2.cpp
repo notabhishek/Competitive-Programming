0ce9520699d42336073a4653f52981f85dda3817
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-07-01 14:04:31 +0530

    Create 1520F2.cpp

---
+++ Standard BIT problems/1520F2.cpp
@@ -0,0 +1,275 @@
+// Problem: F2. Guess the K-th Zero (Hard version)
+// Contest: Codeforces - Codeforces Round #719 (Div. 3)
+// URL: https://codeforces.com/contest/1520/problem/F2
+// Memory Limit: 256 MB
+// Time Limit: 4000 ms
+
+// Code by : Abhishek Tiwari @devil_within
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define all(v) (v).begin(), (v).end()
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define vt vector<int>
+#define pii pair<int, int>
+#define ar array
+using namespace std;
+
+using namespace __gnu_pbds;
+template <class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define int int64_t
+using ll = long long int;
+using ld = long double;
+
+mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
+ll randint(ll a, ll b)
+{
+    return uniform_int_distribution<ll>(a, b)(mt_rng);
+}
+
+/*******************************************************************************************/
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v);
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v);
+template <typename T> inline istream &operator>>(istream &in, vector<T> &a);
+/*******************************************************************************************/
+// N dimensional Vector, Vec<n , int>
+template <int D, typename T> struct Vec : public vector<Vec<D - 1, T>>
+{
+    static_assert(D >= 1, "Vector dimension must be greater than zero!");
+    template <typename... Args> Vec(int n = 0, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...))
+    {
+    }
+};
+template <typename T> struct Vec<1, T> : public vector<T>
+{
+    Vec(int n = 0, const T &val = T()) : vector<T>(n, val)
+    {
+    }
+};
+/*******************************************************************************************/
+template<typename T>
+void __p(T a) { cout << a << " "; }
+template<typename T>
+void __p(std::vector<T> a) { cout << "{ "; for (auto p : a) __p(p); cout << "}"; }
+template<typename T, typename F>
+void __p(pair<T, F> a) { cout << "{ "; __p(a.first); __p(a.second); cout << "}"; }
+template<typename T, typename ...Arg>
+void __p(T a1, Arg ...a) { __p(a1); __p(a...); }
+template<typename Arg1>
+void __f(const char *name, Arg1 &&arg1) {
+    cout<<name<<" : ";__p(arg1);cout<<endl;
+}
+template<typename Arg1, typename ... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+    int bracket=0,i=0;
+    for(; ;i++)
+        if(names[i]==','&&bracket==0)
+            break;
+        else if(names[i]=='(')
+            bracket++;
+        else if(names[i]==')')
+            bracket--;
+    const char *comma=names+i;
+    cout.write(names,comma-names)<<" : ";
+    __p(arg1);
+    cout<<"| ";
+    __f(comma+1,args...);
+}
+
+#define trace(...) //cout <<"L:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
+
+const ll INF = LONG_LONG_MAX;
+const int MOD = 1e9 + 7;
+
+ll fpow(ll b, ll e, ll mod=MOD) {
+    ll ans=1; for(;e;b=b*b%mod,e/=2) if(e&1) ans=ans*b%mod; return ans; }
+
+struct BIT
+{
+	int N;
+	vector<int> bit;
+
+	void init(int n)
+	{
+		N = n;
+		bit.assign(n + 1, 0);
+	}
+
+	void update(int idx, int val)
+	{
+		while(idx <= N)
+		{
+			bit[idx] += val;
+			idx += idx & -idx;
+		}
+	}
+
+	int pref(int idx)
+	{
+		int ans = 0;
+		while(idx > 0)
+		{
+			ans += bit[idx];
+			idx -= idx & -idx;
+		}
+		return ans;
+	}
+
+	int rsum(int l, int r)
+	{
+		return pref(r) - pref(l - 1);
+	}
+
+	//returns first index such that pref[idx] >= val
+
+	int search(int val)
+	{
+		int curSum = 0, pos = 0;
+		for(int i = log2(N) + 1; i >= 0; i--)
+		{
+			if(pos + (1 << i) < N && curSum + bit[pos + (1 << i)] < val)
+			{
+				curSum += bit[pos + (1 << i)];
+				pos += (1 << i);
+			}
+		}
+		return pos + 1;
+	}
+
+	void updateMax(int idx, int val)
+	{
+		while(idx <= N)
+		{
+			bit[idx] = max(bit[idx], val);
+			idx += idx & -idx;
+		}
+	}
+
+	int prefMax(int idx)
+	{
+		int ans = -2e9;
+		while(idx > 0)
+		{
+			ans = max(ans, bit[idx]);
+			idx -= idx & -idx;
+		}
+		return ans;
+	}
+};
+
+void solve()
+{
+	int n , t;
+	cin >> n >>  t;	
+	// to store prefix sums for 0->1 comversions
+	BIT bit;
+	bit.init(n);
+	
+	// to store pref sums for already asked queries
+	vector<int> cache(n+1, -1);
+	while(t-- ) {
+		int k ;
+		cin >> k;
+		int ans = -1;
+		int lo = 1 , hi = n;
+		while(lo <= hi) {
+			int sum , mid = (lo + hi)/2;
+			
+			if(cache[mid] == -1) {
+				//new query
+				cout << "? " << 1 << " " << mid << endl;
+				cin >> sum;
+				//store sum if 0->1 didn't take place
+				cache[mid] = sum - bit.pref(mid);	
+			} else {
+				// add bit.pref to counter 0->1
+				sum = cache[mid] + bit.pref(mid);
+			}
+			if( sum <= (mid - k) ) {
+				ans = mid;
+				hi = mid-1;
+			} else if(sum > (mid-k)) {
+				lo = mid+1;
+			} 
+		}
+		bit.update(ans , 1);
+		cout << "! " << ans << endl;
+	}
+}
+
+signed main()
+{
+    // freopen("input.txt" , "r" , stdin);freopen("output.txt" , "w" ,stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // cout << fixed << setprecision(12);
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; ++tc)
+    {
+        // cout << "Case #"<<tc<<": \n";
+        solve();
+    }
+}
+
+// Functions
+
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (int i = 0; i < (int)v.size(); ++i)
+    {
+        if (i)
+            os << ", ";
+        os << v[i];
+    }
+    os << "]\n";
+    return os;
+}
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v)
+{
+    os << "{";
+    for (auto it : v)
+    {
+        os << it;
+        if (it != *v.rbegin())
+            os << ", ";
+    }
+    os << "}\n";
+    return os;
+}
+template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v)
+{
+    os << "{";
+    for (auto it : v)
+    {
+        os << "(" << it.first << " : " << it.second << ")";
+        if (it != *v.rbegin())
+            os << ", ";
+    }
+    os << "}\n";
+    return os;
+}
+template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v)
+{
+    os << "(";
+    os << v.first << ", " << v.second << ")";
+    return os;
+}
+template <typename T> inline istream &operator>>(istream &in, vector<T> &a)
+{
+    for (auto &x : a)
+        in >> x;
+    return in;
+}
