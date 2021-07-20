71a4e45e7d9344878abef53c8d62f43120819d89
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-07-20 17:23:17 +0530

    Update Longest Valid Parentheses.cpp

--- Dynamic Programming/LeetCode/Longest Valid Parentheses.cpp
+++ Dynamic Programming/LeetCode/Longest Valid Parentheses.cpp
@@ -7,20 +7,42 @@ public:
             1.  validSubstring + ()
             2. validSubstring1 ( validSubstring2 )
         */
-        int n = s.length();
-        vector<int> dp( n, 0);
-        int ans = 0;
-        for(int i = 1; i < n; ++i) {
-            if(s[i] == ')') {
-                // case1   substr ()
-                if(s[i-1] == '(') dp[i] = max(dp[i] , 2 + (i>1 ? dp[i-2] : 0));
-                // case2   ( substr )
-                int idx = i - dp[i-1] - 1;
-                if(s[i-1] == ')' && idx>=0 && s[idx] == '(') {
-                    int cur = 2 + dp[i-1] + ( idx > 0 ? dp[idx-1] : 0);
-                    dp[i] = max(dp[i], cur); 
+        // int n = s.length();
+        // vector<int> dp( n, 0);
+        // int ans = 0;
+        // for(int i = 1; i < n; ++i) {
+        //     if(s[i] == ')') {
+        //         // case1   substr ()
+        //         if(s[i-1] == '(') dp[i] = max(dp[i] , 2 + (i>1 ? dp[i-2] : 0));
+        //         // case2   ( substr )
+        //         int idx = i - dp[i-1] - 1;
+        //         if(s[i-1] == ')' && idx>=0 && s[idx] == '(') {
+        //             int cur = 2 + dp[i-1] + ( idx > 0 ? dp[idx-1] : 0);
+        //             dp[i] = max(dp[i], cur); 
+        //         }
+        //         ans = max(ans, dp[i]);
+        //     }
+        // }
+        
+        
+        /*
+        Stack approach
+        1. push (
+        2. ) -> pop 1 ( and  ans = max(ans, st.top() - i)
+        */
+        int n = s.length() , ans = 0;
+        stack<int> st;
+        st.push(-1);
+        for(int i = 0; i < n; ++i) {
+            if(s[i] == '(')
+                st.push(i);
+            else {
+                st.pop();
+                if(st.empty()) {
+                   st.push(i); 
+                } else {
+                    ans = max(ans, i - st.top());
                 }
-                ans = max(ans, dp[i]);
             }
         }
         return ans;
