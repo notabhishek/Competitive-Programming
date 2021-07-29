71617d054e087a3b3768bc3aed183108fc143e91
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-07-30 04:16:41 +0530

    Update Subgraph_in_treeDP.cpp

--- Dynamic Programming/Standard DP Problems/Subgraph_in_treeDP.cpp
+++ Dynamic Programming/Standard DP Problems/Subgraph_in_treeDP.cpp
@@ -1,105 +1,20 @@
-// Problem: F. Maximum White Subtree
-// Contest: Codeforces - Codeforces Round #627 (Div. 3)
-// URL: https://codeforces.com/contest/1324/problem/F
-// Memory Limit: 256 MB
-// Time Limit: 2000 ms
-
-// Code by : Abhishek Tiwari @devil_within
-#pragma GCC optimize("Ofast")  
-#pragma GCC target("avx,avx2,fma") 
-
+/*
+Given a graph ( N = 1e5 ) having N-1 bi-directional edges
+every node is either 0, or 1
+The value of a subgraph is abs( count(0) - count(1) ) , subgraph has to be connected
+find maximum value of any subgraph, and print nodes in the subgraph
+*/
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#define all(v) (v).begin(), (v).end()
-#define mp make_pair
 #define pb push_back
-#define pf push_front
-#define ff first
-#define ss second
-#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
-#define vt vector<int>
-#define pii pair<int, int>
-#define ar array
 using namespace std;
 
-using namespace __gnu_pbds;
-template <class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-#define int int64_t
-using ll = long long int;
-using ld = long double;
-
-mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
-ll randint(ll a, ll b)
-{
-    return uniform_int_distribution<ll>(a, b)(mt_rng);
-}
-
-/*******************************************************************************************/
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &v);
-template <typename T> ostream &operator<<(ostream &os, const set<T> &v);
-template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v);
-template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v);
-template <typename T> inline istream &operator>>(istream &in, vector<T> &a);
-/*******************************************************************************************/
-// N dimensional Vector, Vec<n , int>
-template <int D, typename T> struct Vec : public vector<Vec<D - 1, T>>
-{
-    static_assert(D >= 1, "Vector dimension must be greater than zero!");
-    template <typename... Args> Vec(int n = 0, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...))
-    {
-    }
-};
-template <typename T> struct Vec<1, T> : public vector<T>
-{
-    Vec(int n = 0, const T &val = T()) : vector<T>(n, val)
-    {
-    }
-};
-/*******************************************************************************************/
-template<typename T>
-void __p(T a) { cout << a << " "; }
-template<typename T>
-void __p(std::vector<T> a) { cout << "{ "; for (auto p : a) __p(p); cout << "}"; }
-template<typename T, typename F>
-void __p(pair<T, F> a) { cout << "{ "; __p(a.first); __p(a.second); cout << "}"; }
-template<typename T, typename ...Arg>
-void __p(T a1, Arg ...a) { __p(a1); __p(a...); }
-template<typename Arg1>
-void __f(const char *name, Arg1 &&arg1) {
-    cout<<name<<" : ";__p(arg1);cout<<endl;
-}
-template<typename Arg1, typename ... Args>
-void __f(const char *names, Arg1 &&arg1, Args &&... args) {
-    int bracket=0,i=0;
-    for(; ;i++)
-        if(names[i]==','&&bracket==0)
-            break;
-        else if(names[i]=='(')
-            bracket++;
-        else if(names[i]==')')
-            bracket--;
-    const char *comma=names+i;
-    cout.write(names,comma-names)<<" : ";
-    __p(arg1);
-    cout<<"| ";
-    __f(comma+1,args...);
-}
-
-#define trace(...) //cout <<"L:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
-
-const ll INF = LONG_LONG_MAX;
-const int MOD = 1e9 + 7;
-
-ll fpow(ll b, ll e, ll mod=MOD) {
-    ll ans=1; for(;e;b=b*b%mod,e/=2) if(e&1) ans=ans*b%mod; return ans; }
-
+const int INF = 1e9;
 vector<vector<int>> g;
 int n;
 vector<int> a;
 vector<int> down; // max in subtree
 vector<int> up; // max in everything except subtree
+vector<int> par;
 // ans[i] = up[i] + down[i]
 /*
 	 u
@@ -111,8 +26,9 @@ down[u] =  a[u] + sum ( down[children] )
 up[u] = up[par[u]] + down[par[u]] - down[c]
 */
 int calcDown(int u, int p) {
-	if(down[u] != -1)
+	if(down[u] != -INF)
 		return down[u];
+	par[u] = p;
 	for(auto &i : g[u]) 
 		if(i!=p) calcDown(i, u);
 	down[u] = (a[u] ? 1 : -1);
@@ -121,18 +37,58 @@ int calcDown(int u, int p) {
 	return down[u];
 }
 int calcUp(int u, int p) {
-	if(up[u] != -1)
+	if(up[u] != -INF)
 		return up[u];
 	up[u] = (p!=-1 ? max((int)0, up[p] + down[p] - max((int)0, down[u])) : 0);
 	for(auto &i : g[u])
 		if(i != p) calcUp(i, u);
 	return up[u];
 }
-void solve()
+vector<int> path;
+void fDPath(int u, int p){
+	path.push_back(u);
+	for(auto &v: g[u]) 
+		if(v!=p && down[v] > 0)
+			fDPath(v, u);
+}
+void fUPath(int u, int c){
+	if(u == -1)
+		return;
+	int upCont = up[u] + down[u] - max((int)0, down[c]);
+	if(upCont > 0) {
+		path.push_back(u);
+	}
+	for(auto &i : g[u]) 
+		if(i != c && i!=par[u]) 
+			fDPath(i , u);
+	fUPath(par[u], u);
+}
+int solve()
 {
-	cin >> n;
+	down = vector<int>(n+1, -INF);
+	up = vector<int>(n+1, -INF);
+	calcDown(1, -1);
+	calcUp(1, -1);
+	int ans = -INF , node = 1;
+	for(int i = 1; i <= n; ++i)  {
+		if(up[i] + down[i] > ans) {
+			ans = up[i]+down[i];
+			node = i;
+		}
+	}
+	fDPath(node, par[node]);
+	fUPath(par[node], node);
+	return ans;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
 	g = vector< vector<int>> (n+1);
-	a.resize(n + 1);
+	a.resize(n+1);
+	par.resize(n+1);
 	for(int i = 1; i <= n; ++i) cin >> a[i];	
 	for(int i = 1; i < n; ++i) {
 		int u , v;
@@ -140,75 +96,19 @@ void solve()
 		g[u].pb(v);
 		g[v].pb(u);	
 	}
-	down.resize(n+1, -1);
-	up.resize(n+1, -1);
-	calcDown(1, -1);
-	calcUp(1, -1);
-	for(int i = 1; i <= n; ++i) cout << down[i]+up[i] << " ";
-}
-
-signed main()
-{
-    // freopen("input.txt" , "r" , stdin);freopen("output.txt" , "w" ,stdout);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    // cout << fixed << setprecision(12);
-    int T = 1;
-    // cin >> T;
-    for (int tc = 1; tc <= T; ++tc)
-    {
-        // cout << "Case #"<<tc<<": \n";
-        solve();
+	
+    int ans1 = solve();
+    vector<int> path1 = path;
+	for(auto &i : a) i= 1-i;
+	int ans2 = solve();
+    vector<int> path2 = path;
+    if(ans2 > ans1) {
+    	swap(path1, path2);
+    	ans1 = ans2;
     }
-}
-
-// Functions
-
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &v)
-{
-    os << "[";
-    for (int i = 0; i < (int)v.size(); ++i)
-    {
-        if (i)
-            os << ", ";
-        os << v[i];
-    }
-    os << "]\n";
-    return os;
-}
-template <typename T> ostream &operator<<(ostream &os, const set<T> &v)
-{
-    os << "{";
-    for (auto it : v)
-    {
-        os << it;
-        if (it != *v.rbegin())
-            os << ", ";
-    }
-    os << "}\n";
-    return os;
-}
-template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v)
-{
-    os << "{";
-    for (auto it : v)
-    {
-        os << "(" << it.first << " : " << it.second << ")";
-        if (it != *v.rbegin())
-            os << ", ";
-    }
-    os << "}\n";
-    return os;
-}
-template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v)
-{
-    os << "(";
-    os << v.first << ", " << v.second << ")";
-    return os;
-}
-template <typename T> inline istream &operator>>(istream &in, vector<T> &a)
-{
-    for (auto &x : a)
-        in >> x;
-    return in;
+    cout << ans1 << "\n";
+    set<int> ps(path1.begin(), path1.end());
+    cout << ps.size() << "\n";
+    for(auto &i : ps) cout << i << " ";
+    cout << "\n";
 }
