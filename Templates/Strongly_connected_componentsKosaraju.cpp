86453e538773d7d5b5b8a3aebba3952dcc1f5cc1
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-08-18 14:06:02 +0530

    Update Strongly_connected_componentsKosaraju.cpp

--- Templates/Strongly_connected_componentsKosaraju.cpp
+++ Templates/Strongly_connected_componentsKosaraju.cpp
@@ -1,10 +1,11 @@
+// strongly connected components, condensation graph
 #include<bits/stdc++.h>
 using namespace std;
 #define int int64_t 
 
 const int MAXN = 1e5 + 1;
-vector<int> adj[MAXN];
-vector<int> tadj[MAXN];
+vector<int> g[MAXN];
+vector<int> tg[MAXN];
 
 vector<int> vis;
 vector<int> order;
@@ -12,9 +13,15 @@ int comp[MAXN];
 int sccs = 0;
 vector< vector< int > >  SCC;
 
+// for condensation graph
+vector<int> roots;
+vector<int> root_nodes;
+vector<vector<int>> cg;
+
+
 void dfsOrder(int src) {
 	vis[src] = true;
-	for(auto to : adj[src] ) {
+	for(auto to : g[src] ) {
 		if(!vis[to]) {
 			dfsOrder(to);
 		}
@@ -25,7 +32,7 @@ void dfsOrder(int src) {
 // dfs on transpose
 void dfsSCC(int src) {
 	vis[src] = true;
-	for(auto to : tadj[src]) {
+	for(auto to : tg[src]) {
 		if(!vis[to]) {
 			dfsSCC(to);		
 		}
@@ -36,7 +43,10 @@ void dfsSCC(int src) {
 
 void kosaraju(int n) {
 	order.clear();
-	vis.assign(n + 1 , false);
+	vis.assign(n+1, false);
+	roots.assign(n+1, 0);
+	root_nodes.clear();
+	cg.clear(); cg.resize(n+1);
 	
 	for(int node = 1 ; node <= n; ++node )
 		if(!vis[node]) 
@@ -50,6 +60,10 @@ void kosaraju(int n) {
 		if(!vis[ order[n - i] ])  {
 			SCC.push_back(vector<int> ());
 			dfsSCC( order[ n - i ]);
+			
+			int root = SCC[sccs].front(); root_nodes.push_back(root);
+			for(auto &x : SCC[sccs]) roots[x]=root;
+			
 			++sccs;
 		}
 	}
@@ -61,6 +75,20 @@ void kosaraju(int n) {
 	}
 	cout << "Component Numbers : \n";
 	for(int i = 1; i <= n; ++i) cout << comp[i] << " "; cout << "\n";
+	
+	// Condensation graph
+	for(int u = 1; u <= n; ++u) {
+		for(auto &v: g[u]) {
+			if(roots[u] != roots[v]) 
+				cg[u].push_back(v);
+		}
+	}
+	cout << "root nodes : "; for(auto &i: root_nodes) cout << i << " "; cout << "\n";
+	cout << "roots : "; for(auto &i: roots) cout << i << " "; cout << "\n";
+	cout << "condensation graph: \n";
+	for(int i = 1; i <= n; ++i) {
+		cout << i << ": "; for(auto &j : cg[i]) cout << j << " "; cout << "\n";
+	}
 }
 
 int32_t main() {
@@ -70,10 +98,9 @@ int32_t main() {
 	for(int i = 0 ; i < m; ++i) {
 		int u , v;
 		cin >> u >> v;
-		adj[u].push_back(v);
-		tadj[v].push_back(u);
+		g[u].push_back(v);
+		tg[v].push_back(u);
 	}
 	
 	kosaraju(n);
-	
 }
