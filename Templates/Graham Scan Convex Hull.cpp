addb3f9fabb3091176a39695625ca0d22701bbfc
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-09-03 19:09:19 +0530

    Create Graham Scan Convex Hull.cpp

---
+++ Templates/Graham Scan Convex Hull.cpp
@@ -0,0 +1,106 @@
+struct point
+{
+	int x, y; // change to double if needed, also change set<pair<int,int>> in convext_hull
+
+	point () {}
+
+	point(int x, int y) : x(x), y(y) {}
+
+	void operator =(const point &p)
+	{
+		x=p.x, y=p.y;
+	}
+
+	bool operator <(const point&p)
+	{
+		if(x==p.x)
+			return y<p.y;
+		return x<p.x;
+	}
+
+	point operator +(const point&p) const
+	{
+		point pt(x + p.x, y + p.y);
+		return pt;
+	}
+
+	point operator -(const point&p) const
+	{
+		point pt(x - p.x, y - p.y);
+		return pt;
+	}
+
+	double crossProduct(const point &p) const
+	{	
+		return x * p.y - y * p.x;
+	}	
+
+	int dotProduct(const point &p) const
+	{
+		return x * p.x + y * p.y;
+	}
+
+	double dist()
+	{
+		return x*x + y*y;
+	}
+};
+
+
+bool comp(point &p1, point &p2)
+{
+	if(p1.x!=p2.x)
+		return p1.x<p2.x;
+	return p1.y<p2.y;
+}
+
+bool cw(point &a, point &b, point &c)
+{
+	int area=a.x*(b.y-c.y) + b.x*(c.y-a.y) + c.x*(a.y-b.y);
+	return area<=0; // remove = if you don't want collinear points
+}
+
+bool ccw(point &a, point &b, point &c)
+{
+	int area=a.x*(b.y-c.y) + b.x*(c.y-a.y) + c.x*(a.y-b.y);
+	return area>=0; // remove = if you don't want collinear points
+}
+
+vector<point> convex_hull(vector<point> &v)
+{
+	if(v.size()==1)
+		return v;
+
+	sort(v.begin(), v.end(), comp);
+
+	point p1=v[0], p2=v.back();
+
+	vector<point> up, down;
+	up.push_back(p1);
+	down.push_back(p1);
+    
+	for(int i=1;i<v.size();i++)
+	{
+		if(i==v.size()-1 || cw(p1, v[i], p2))
+		{
+			while(up.size()>=2 && !cw(up[up.size()-2], up[up.size()-1], v[i]))
+				up.pop_back();
+			up.push_back(v[i]);
+		}
+		if(i==v.size()-1 || ccw(p1, v[i], p2))
+		{
+			while(down.size()>=2 && !ccw(down[down.size()-2], down[down.size()-1], v[i]))
+				down.pop_back();
+			down.push_back(v[i]);
+		}
+	}
+	
+    set<pair<int,int>> taken;
+    for(auto &i : up) taken.insert({i.x, i.y});
+    
+	for(int i=down.size()-2;i>0;i--) // to remove duplicates in case of collinear points that come in both hulls
+        if(!taken.count({down[i].x, down[i].y}))
+		    up.push_back(down[i]);
+
+	return up;
+}
