62b41d0876d7224ada72d0669fa08e70cf8f6978
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-09-11 17:14:07 +0530

    Create Max Flow Edmonds-Karp.cpp

---
+++ Templates/Max Flow Edmonds-Karp.cpp
@@ -0,0 +1,70 @@
+#define int int64_t
+const int INF = 1e18;
+int n;
+vector<vector<int>> cap;
+vector<vector<int>> g;
+vector<set<int>> setg; // to check multiple edges
+
+int bfs(int s, int t, vector<int>& par) {
+    fill(par.begin(), par.end(), -1);
+    par[s] = -2;
+    queue<pair<int, int>> q;
+    q.push({s, INF});
+
+    while (!q.empty()) {
+        int u = q.front().first;
+        int flow = q.front().second;
+        q.pop();
+
+        for (int v : g[u]) {
+            if (par[v] == -1 && cap[u][v]) {
+                par[v] = u;
+                int new_flow = min(flow, cap[u][v]);
+                if (v == t) return new_flow;
+                q.push({v, new_flow});
+            }
+        }
+    }
+
+    return 0;
+}
+
+int maxflow(int s, int t) {
+    int flow = 0;
+    vector<int> par(n+1);
+    int new_flow;
+
+    while (new_flow = bfs(s, t, par)) {
+        flow += new_flow;
+        int u = t;
+        while (u != s) {
+            int prev = par[u];
+            cap[prev][u] -= new_flow;
+            cap[u][prev] += new_flow;
+            u = prev;
+        }
+    }
+
+    return flow;
+}
+
+void solve()
+{
+	int m;
+	cin >> n >> m;
+	g.resize(n+1);
+	setg.resize(n+1);
+	cap.assign(n+1, vector<int>(n+1, 0));
+	for(int i=0; i < m; ++i) {
+		int u, v, c;
+		cin >> u >> v >> c;
+		if(setg[u].count(v) == 0) { // multiple pipes
+			g[u].push_back(v);
+			g[v].push_back(u);
+			setg[u].insert(v);
+			setg[v].insert(u);
+		}
+		cap[u][v] += c;
+	}
+	cout << maxflow(1, n) << "\n";
+}
