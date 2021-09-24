18fb482f48301e7ead3e9f7408f21a539bff695b
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-09-24 13:08:11 +0530

    Create N-th Tribonacci Number.cpp

---
+++ Leetcode Daily Challenges/N-th Tribonacci Number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n+3);
+        dp[0] = 0;
+        dp[1] = dp[2] = 1;
+        for(int i = 3; i  <= n; ++i) dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        return dp[n];
+    }
+};
