7c11c9927529a4c8a31b4f263b8bc6c0ab7d4ba3
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-10-15 15:29:24 +0530

    Update Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp

--- Interview Prep/Google/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
+++ Interview Prep/Google/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -30,3 +30,41 @@ public:
 };
 
 // Solution 2: using MaxDeque and MinDeque O(N)
+// pre-requisite: https://github.com/notabhishek/Competitive-Programming/blob/main/Interview%20Prep/Google/Sliding%20Window%20Maximum.cpp
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        
+        deque<int> maxDeque, minDeque; // to maintain max, min of current window
+        
+        int ans = 1;
+        int i = 0, j = 0, n = nums.size(); // minDeque index
+        int diff = 0;
+        while(i < n-1) {
+            
+            // expand current window until difference > limit
+            while(j < n && diff <= limit) {
+                while(!maxDeque.empty() && nums[maxDeque.back()] <= nums[j]) {
+                    maxDeque.pop_back();
+                }
+                maxDeque.push_back(j);
+
+                // push in minDeque
+                while(!minDeque.empty() && nums[minDeque.back()] >= nums[j]) {
+                    minDeque.pop_back();
+                }
+                minDeque.push_back(j);
+                diff = nums[maxDeque.front()] - nums[minDeque.front()];
+                if(diff <= limit) ans = max(ans, j-i+1);
+                ++j;
+            }
+            // now diff > limit, erase i
+            if(maxDeque.front() == i) maxDeque.pop_front();
+            if(minDeque.front() == i) minDeque.pop_front();
+
+            diff = nums[maxDeque.front()] - nums[minDeque.front()];
+            ++i;
+        }
+        return ans;
+    }
+};
