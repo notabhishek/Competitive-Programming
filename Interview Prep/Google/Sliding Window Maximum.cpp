3725afb3a3f6442d8608541a35102aa95016fe67
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-10-15 15:02:44 +0530

    Update Sliding Window Maximum.cpp

--- Interview Prep/Google/Sliding Window Maximum.cpp
+++ Interview Prep/Google/Sliding Window Maximum.cpp
@@ -1,39 +1,7 @@
-// Solution1: Multiset nlogk
-// Solution2: Heap: nlogn 
-// Solution3: MaxDeque: n
-
+// Solution1: Heap: nlogn 
+// Solution2: MaxDeque: n
 
 // Solution 1
-// Multiset solution: NlogK
-class Solution {
-public:
-    int longestSubarray(vector<int>& nums, int limit) {
-        multiset<int> window;
-        int i = 0, j = 0, n = nums.size();
-        int ans = 0;
-        while(i < n) {
-            int diff = 0;
-            while(j < n && diff <= limit) {
-                window.insert(nums[j]);
-                diff = (*window.rbegin()) - (*window.begin());
-                ++j;
-            }
-            if(diff > limit) {
-                --j;
-                window.erase(window.find(nums[j]));
-                diff = (*window.rbegin()) - (*window.begin());
-            }
-            if(diff <= limit) ans = max(ans, j - i);
-            window.erase(window.find(nums[i]));
-            ++i;
-        }
-        return ans;
-    }
-};
-
-
-
-// Solution 2
 // By Implementing heap of pairs {data,index}: NlogN
 struct heapNode {
 	int data, index;
@@ -96,7 +64,7 @@ public:
 };
 
 
-// Solution 3
+// Solution 2
 // Using Max Deque: O(N)
 class Solution {
 public:
