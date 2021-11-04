8e47f5721003b1dc80ad1533e89ef7287f20a035
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-11-04 11:56:38 +0530

    Create Sum of Left Leaves.cpp

---
+++ Leetcode Daily Challenges/Sum of Left Leaves.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(!root) return 0;
+        int ans = sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+        if(root->left && !root->left->left && !root->left->right) ans += root->left->val;
+        return ans;
+    }
+};
