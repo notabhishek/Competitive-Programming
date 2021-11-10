79d59f3fcbb2902e275856b58e867bb1481fc26e
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2021-11-10 13:25:18 +0530

    Create Best Time to Buy and Sell Stock II.cpp

---
+++ Leetcode Daily Challenges/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        for(int i = 1; i < prices.size(); ++i) 
+            profit += max(0, prices[i]- prices[i-1]);
+        return profit;
+    }
+};
