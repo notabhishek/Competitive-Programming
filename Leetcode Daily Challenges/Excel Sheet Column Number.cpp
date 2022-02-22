ddece7da2c33ab5f564bfeb8c573f27cd1d3606e
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2022-02-22 12:15:35 +0530

    Create Excel Sheet Column Number.cpp

---
+++ Leetcode Daily Challenges/Excel Sheet Column Number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int ans = 0;
+        for(char c : columnTitle) {
+            ans = ans * 26 + (c - 'A' + 1);
+        }
+        return ans;
+    }
+};
