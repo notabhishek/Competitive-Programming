cfb4a70cecc6431772e1236368a499752ba0b66c
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2022-03-01 12:56:47 +0530

    Create Counting Bits.cpp

---
+++ Leetcode Daily Challenges/Counting Bits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        for(int i = 0; i <= n; ++i)
+            ans[i] = __builtin_popcount(i);
+        return ans;
+    }
+};Counting Bits
