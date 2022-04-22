3570c8cead00e5f6b4741143c116b47676fe1095
Abhishek Tiwari <39875465+notabhishek@users.noreply.github.com>
2022-04-22 09:04:25 +0530

    Create Design HashMap.cpp

---
+++ Leetcode Daily Challenges/Design HashMap.cpp
@@ -0,0 +1,27 @@
+class MyHashMap {
+public:
+    vector<int> hash;
+    MyHashMap() {
+        hash.assign(1e6+1, -1);
+    }
+    
+    void put(int key, int value) {
+        hash[key] = value;
+    }
+    
+    int get(int key) {
+        return hash[key];
+    }
+    
+    void remove(int key) {
+        hash[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
